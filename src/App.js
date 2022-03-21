c053c36945f320fdcd683dfe7d04c0aecd15e248
koreylombardi17 <koreylombardi17@yahoo.com>
2022-03-21 00:55:13 -0400

    Added tensorflow to detect humans. required: npm install @tensorflow/tfjs-node and npm install @tensorflow-models/coco-ssd

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import './App.css';
-import React, { Component } from 'react';
-import Home from './pages/home';
+import "./App.css";
+import React, { Component } from "react";
+import Home from "./pages/Home";
 
-class App extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-  
-  render()
-  {
-    return (
-      <div className="App">
-        <Home></Home>
-      </div>
-    )
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Home></Home>
+    </div>
+  );
+};
 export default App;
